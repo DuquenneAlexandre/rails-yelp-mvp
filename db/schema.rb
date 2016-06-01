eddaf5b7d7851ca57991a58b0129c43ffcf7bf9f
Alexandre Duquenne <duquenne.alexandre22@gmail.com>
2016-06-01 15:00:58 +0200

    three

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160601120153) do
+ActiveRecord::Schema.define(version: 20160601121008) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20160601120153) do
     t.string   "category"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
